01139c4651afb6f09b8e085cd98d4f1bf1347f1d
lvenlong <lvenlong@baidu.com>
2019-01-16 12:03:26 +0800

    新增vector

--- sync_vector.h
+++ sync_vector.h
@@ -53,8 +53,58 @@ namespace bigpipe
         //从同步vector中取元素。如果同步vector为空，将阻塞指定时长;BGCC_SEMA_WAIT_INFINITE表示无限阻塞
         int32_t get(ElemType& elem, int millisecond)
         {
-            _rwlock_clear.get_rdlock()
+            _rwlock_clear.get_rdlock();
+            int32_t ret = _psem->wait(millisecond);
+
+            if(0 == ret){
+                {
+                    ScopedGuard guard(&_mutex);
+                    if(guard.is_locked()){
+                        elem = _queue.front();
+                        _queue.pop();
+                    }
+                    else{
+                        _relock_clear.unlock();
+                        return E_BIGPIPE_SYSERROR;
+                    }
+                }
+                _rwlock_clear.unlock();
+                return 0;
+            }
+            else{
+                _rwlock_clear.unlock();
+                return ret;
+            }
         }
+
+        int32_t size()
+        {
+            return (int32_t)_queue.size();
+        }
+
+        //清空
+        void clear()
+        {
+            sc_trace("vector clear");
+            _rwlock_clear.get_wrlock();
+            ScopedGuard guard(&_mutex);
+            std::vector<ElemType> tmp;
+            while(_queue.size()){
+                _queue.pop();
+            }
+            delete _psem;
+            _psem = new Semaphore();
+            _rwlock_clear.unlock();
+        }
+
+    protected:
+        SyncVector(const SyncVector&);
+        SyncVector& operator=(const SyncVector&);
+    private:
+        std::vector<ElemType> _queue;
+        Mutex _mutex;
+        RWLock _rwlock_clear;
+        Semaphore *_psem;
     };
 }
 
